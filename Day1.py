b2e2c07acf5b784b84d41814e097feee19f2952b
DMGamage <gamagemadhushan@gmail.com>
2023-11-30 09:48:16 +0530

    new

--- Day1.py
+++ Day1.py
@@ -1,1 +1,18 @@
-New
+class Product:
+    def __init__(self,price):
+        self.__price=price
+
+    def get_price(self):
+        return self.__price
+
+    def set_price(self,value):
+        if value <0:
+            raise ValueError("The value has negative count")
+        self.__price=value
+
+
+    price=property(get_price,set_price)
+
+product=Product(10)
+
+ffdf
